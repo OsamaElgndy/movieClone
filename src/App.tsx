5e7af6573841d38cd036f40bac6866873781e728
osama elgendy <osam elgend@gmail.com>
2025-01-04 11:50:00 +0200

    create component sort by

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Search from './module/components/1-search/Search'
 import List from './module/components/2-list/List'
-import CartMovies from './module/components/3-cardMovies/CardMovies'
+import CartMovies from './module/components/3-cardMovies/CardMovies.tsx'
 function App() {
 
   return (
---
+++ src/module/components/2-list/List.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export default function List() {
+const [sortBy, setSortBy] = useState<number>(0);
+ return (
+    <div className="ml-7 mt-12 flex  gap-2">
+  
+  <h1 className="text-2xl text-white capitalize">sort by:</h1>
+  <div className="flex gap-2">
+  <button className=" capitalize p-[5px] bg-[#FAEFD9]  font-medium text-black rounded-md hover:bg-[#f7dcbf]">
+  years
+</button>
+    <button className=" capitalize p-[5px] bg-[#FAEFD9]  font-medium text-black rounded-md hover:bg-[#f7dcbf]">rating</button>
+    <button  className=" capitalize  p-[5px] bg-[#FAEFD9]  font-medium text-black rounded-md hover:bg-[#f7dcbf]">length</button>
+  </div>
+
+    </div>
+  )
+}
